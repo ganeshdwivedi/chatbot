3ee22cb110e6475bc3dfa14e209907ed813c76d4
Ganesh Dwivedi <ganeshdwivedi1783@gmail.com>
2026-02-05 14:45:06 +0530

    updated

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,25 +1,38 @@
+"use client";
 import ChatView from "@/components/ValidationChat";
+import { useSearchParams } from "next/navigation";
+import { Suspense, useEffect, useState } from "react";
 
-type PageProps = {
-  searchParams: {
-    designation?: string;
-    role?: string;
-    field?: string;
-    experience?: string;
-    difficulty?: string;
-  };
+const Page = () => {
+  const [params, setParams] = useState({
+    designation: "",
+    role: "",
+    field: "",
+    experience: "",
+    difficulty: "",
+  });
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+
+    setParams({
+      designation: urlParams.get("designation") || "",
+      role: urlParams.get("role") || "",
+      field: urlParams.get("field") || "",
+      experience: urlParams.get("experience") || "",
+      difficulty: urlParams.get("difficulty") || "",
+    });
+  }, []);
+  return (
+    <Suspense fallback={<div>Loading chat...</div>}>
+      <ChatView
+        designation={params?.designation}
+        role={params?.role}
+        field={params?.field}
+        experience={params?.experience}
+        difficulty={params?.difficulty}
+      />
+    </Suspense>
+  );
 };
 
-export default function Page({ searchParams }: PageProps) {
-  const { designation, role, field, experience, difficulty } = searchParams;
-
-  return (
-    <ChatView
-      designation={designation}
-      role={role}
-      field={field}
-      experience={experience}
-      difficulty={difficulty}
-    />
-  );
-}
+export default Page;
