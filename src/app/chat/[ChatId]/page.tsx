686202793345e17d15e2c9e5a40bc4670a8ec457
Ganesh Dwivedi <ganeshdwivedi1783@gmail.com>
2026-02-05 11:43:45 +0530

    updated

--- src/app/chat/[ChatId]/page.tsx
+++ src/app/chat/[ChatId]/page.tsx
@@ -1,11 +1,28 @@
+"use client";
 import ChatSideBar from "@/components/ChatSideBar";
 import ChatView from "@/components/ValidationChat";
 import { MoreHorizontal } from "lucide-react";
+import { useSearchParams } from "next/navigation";
 import React from "react";
 
 const initialIdea = "NewChat  ";
 const page = () => {
-  return <ChatView />;
+  const searchParams = useSearchParams();
+
+  const designation: any = searchParams.get("designation");
+  const role: any = searchParams.get("role");
+  const field: any = searchParams.get("field");
+  const experience: any = searchParams.get("experience");
+  const difficulty: any = searchParams.get("difficulty");
+  return (
+    <ChatView
+      designation={designation}
+      role={role}
+      field={field}
+      experience={experience}
+      difficulty={difficulty}
+    />
+  );
 };
 
 export default page;
